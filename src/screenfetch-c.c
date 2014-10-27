4be430c5b977b4fbbc8514ff682cc08e95b0a761
William Woodruff <william@tuffbizz.com>
2014-10-27 16:46:23 -0400

    comment for later inclusion

--- src/screenfetch-c.c
+++ src/screenfetch-c.c
@@ -379,7 +379,10 @@ void detect_distro(char *str)
 
 		else if (OS == OSX)
 		{
-			/* possibly read /System/Library/CoreServices/SystemVersion.plist? */
+			/*
+				Use this:
+				https://www.opensource.apple.com/source/DarwinTools/DarwinTools-1/sw_vers.c
+			*/
 			#if defined(__APPLE__) && defined(__MACH__) && __MAC_OS_X_VERSION_MIN_REQUIRED <= 1070
 				int ver_maj, ver_min, ver_bug;
 				Gestalt(gestaltSystemVersionMajor, (SInt32 *) &ver_maj);
@@ -387,7 +390,7 @@ void detect_distro(char *str)
 				Gestalt(gestaltSystemVersionBugFix, (SInt32 *) &ver_bug);
 
 				snprintf(str, MAX_STRLEN, "Max OS X %d.%d.%d", ver_maj, ver_min, ver_bug);
-			#else /* Gestalt is deprecated on 10.8+, so use sw_vers */
+			#elif defined(__APPLE__) && defined(__MACH__) /* Gestalt is deprecated on 10.8+, so use sw_vers */
 				distro_file = popen("sw_vers -productVersion | tr -d '\\n'", "r");
 				fgets(distro_name_str, MAX_STRLEN, distro_file);
 				pclose(distro_file);
