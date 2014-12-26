74e278844a0fad14eda84f068206d88acba72a6a
William Woodruff <william@tuffbizz.com>
2014-12-26 12:46:38 -0500

    reorganize take_screenshot, add call_status variable to check status on system()

--- src/util.c
+++ src/util.c
@@ -56,6 +56,11 @@ void split_uptime(long uptime, int *secs, int *mins, int *hrs, int *days)
 */
 void take_screenshot(bool verbose)
 {
+#if !defined(__CYGWIN__)
+	int call_status = 1;
+	char file_loc[MAX_STRLEN];
+#endif
+
 	printf("%s", "Taking shot in 3..");
 	fflush(stdout);
 	sleep(1);
@@ -67,50 +72,48 @@ void take_screenshot(bool verbose)
 	sleep(1);
 	printf("%s\n", "0");
 
-	#if defined(__CYGWIN__)
-		/* terrible hack, the printscreen key is simulated */
-		keybd_event(VK_SNAPSHOT, 0, 0, 0);
-		
-		if (verbose)
-			VERBOSE_OUT("Screenshot has been saved to the clipboard.", "");
-		
+#if defined(__CYGWIN__)
+	/* terrible hack, the printscreen key is simulated */
+	keybd_event(VK_SNAPSHOT, 0, 0, 0);
 
-		/* NOT FINSISHED - HBITMAP needs to be saved
-		HDC screen_dc = GetDC(NULL);
-		HDC mem_dc = CreateCompatibleDC(screen_dc);
+	if (verbose)
+		VERBOSE_OUT("Screenshot has been saved to the clipboard.", "");
 
-		int horiz = GetDeviceCaps(screen_dc, HORZRES);
-		int vert = GetDeviceCaps(screen_dc, VERTRES);
+	/* NOT FINSISHED - HBITMAP needs to be saved
+	HDC screen_dc = GetDC(NULL);
+	HDC mem_dc = CreateCompatibleDC(screen_dc);
 
-		HBITMAP bitmap = CreateCompatibleBitmap(screen_dc, horiz, vert);
-		HBITMAP old_bitmap = SelectObject(mem_dc, bitmap);
+	int horiz = GetDeviceCaps(screen_dc, HORZRES);
+	int vert = GetDeviceCaps(screen_dc, VERTRES);
 
-		BitBlt(mem_dc, 0, 0, horiz, vert, screen_dc, 0, 0, SRCCOPY);
-		bitmap = SelectObject(mem_dc, old_bitmap);
+	HBITMAP bitmap = CreateCompatibleBitmap(screen_dc, horiz, vert);
+	HBITMAP old_bitmap = SelectObject(mem_dc, bitmap);
 
-		DeleteDC(screen_dc);
-		DeleteDC(mem_dc);
-		*/
-	#elif defined(__APPLE__) && defined(__MACH__)
-			system("screencapture -x ~/screenfetch_screenshot.png 2> /dev/null");
-	#else
-			system("scrot ~/screenfetch_screenshot.png 2> /dev/null");
-	#endif
+	BitBlt(mem_dc, 0, 0, horiz, vert, screen_dc, 0, 0, SRCCOPY);
+	bitmap = SelectObject(mem_dc, old_bitmap);
 
-	#if !defined(__CYGWIN__)
-		char file_loc[MAX_STRLEN];
-		safe_strncpy(file_loc, getenv("HOME"), MAX_STRLEN);
-		strncat(file_loc, "/screenfetch_screenshot.png", MAX_STRLEN);
+	DeleteDC(screen_dc);
+	DeleteDC(mem_dc);
+	*/
+#elif defined(__APPLE__) && defined(__MACH__)
+		call_status = system("screencapture -x ~/screenfetch_screenshot.png 2> /dev/null");
+#else
+		call_status = system("scrot ~/screenfetch_screenshot.png 2> /dev/null");
+#endif
 
-		if (FILE_EXISTS(file_loc) && verbose)
-		{
-			VERBOSE_OUT("Screenshot successfully saved.", "");
-		}
-		else if (verbose)
-		{
-			ERROR_OUT("Error: ", "Problem saving screenshot.");
-		}
-	#endif
+#if !defined(__CYGWIN__)
+	safe_strncpy(file_loc, getenv("HOME"), MAX_STRLEN);
+	strncat(file_loc, "/screenfetch_screenshot.png", MAX_STRLEN);
+
+	if (FILE_EXISTS(file_loc) && verbose)
+	{
+		VERBOSE_OUT("Screenshot successfully saved.", "");
+	}
+	else if (call_status && error)
+	{
+		ERROR_OUT("Error: ", "Problem saving screenshot.");
+	}
+#endif
 
 	return;
 }
